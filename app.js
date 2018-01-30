7da25b58e196f9620933a217f2adec77fa5d741f
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-01-30 16:00:13 +0530

    updates on routes and views

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
+const search = require('./routes/search');
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET search page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Spoyl Project'});
+  res.render('search', {title: 'Spoyl Project'});
 });
 
 module.exports = router;
