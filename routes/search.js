1f8de827e98687e17ca4adfa95ddc392247e1527
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-01-31 10:19:14 +0530

    route updates

--- routes/search.js
+++ routes/search.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const router = express.Router({ mergeParams : true });
+const router = express.Router({mergeParams : true});
+const API = require('../services/ajax');
+//unsplash clientId
+const clientId = '5a419933621f55950741e4c30182c1d5f99974112a96cf0c49d8d9091cc0ba7e';
+let url = 'https://api.unsplash.com/photos?client_id=' + clientId + '';
 
 /* GET search page. */
 router.get('/', (req, res, next) => {
   console.log(req.params);
+  API.AJAX_GET(url).then((data) =>{
+    console.log(data);
+  });
   res.render('search', {
-    title: 'Spoyl Project'
+    title: 'Spoyl Project',
+    query: req.params.searchTerm
   });
 });
 
